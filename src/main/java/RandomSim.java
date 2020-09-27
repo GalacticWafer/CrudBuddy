5e9553064b525e660304fb99925ef8479d8fc963
GalacticWafer <mxhjohnson4dba@gmail.com>
2020-09-27 01:34:55 -0600

    filled in next*() methods, but they need some work to give desired output

--- src/main/java/RandomSim.java
+++ src/main/java/RandomSim.java
@@ -2,60 +2,84 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 
 class RandomSim extends Random {
-	private static final Date PAST_DATE = new Date();
-	public static final int NUMBER_OF_CUSTOMER_FIELDS = 4;
 	private final Crud crud;
-	private final ArrayList<String> prodictIdList = new ArrayList<>();
+	private String[] productIds;
 	private int size;
+	private Scanner emailScanner;
+	private Scanner dates;
+	public static final String emailFileName = "emails.csv";
+	public static final String dateFileName = "dates.csv";
 	
-	public RandomSim(Crud crud) throws SQLException
-	{
-		
+	public RandomSim(Crud crud) throws SQLException {
 		this.crud = crud;
 		size = 0;
 		ResultSet rs = crud.query("select product_id from inventory;");
-		while(rs.next()){
-			prodictIdList.add(rs.getString(1));
+		productIds = new String[rs.getMetaData().getColumnCount()];
+		for(int i = 0; i < productIds.length && rs.next(); i++) {
+			productIds[i] = rs.getString(1);
 			size++;
 		}
+		emailScanner = new Scanner(emailFileName);
+		dates = new Scanner(dateFileName);
 	}
 	
-	
-	public void simulateOrders(String dateQuantityFile) throws FileNotFoundException, SQLException {
+	public void simulateOrders(String dateQuantityFile) throws FileNotFoundException,
+															   SQLException {
 		Scanner scanner = new Scanner(new File(dateQuantityFile));
 		
-		for(int i = 0; scanner.hasNextLine(); i++) {
-			
-			String[] dateAndQuantity = scanner.nextLine().split(",");
-			Date date = new Date(dateAndQuantity[0]);
-			int quantity = Integer.parseInt(dateAndQuantity[1]);
-			Object[] customer = new Object[NUMBER_OF_CUSTOMER_FIELDS];
-			if(nextInt(10) + 1 > 5) {
-				customer = nextNewCustomer();
-			}
-			else {
-				customer = nextOldCustomer();
-			}
+		while(scanner.hasNextLine()) {
 			Order order = new Order();
-			order.setFields(customer, nextProduct(), quantity, date, true);
+			order.setFields(nextCustomer(), nextProduct(), nextQuantity(), nextDate(), true);
 			//TODO: make sure it doesn't try to send emails for this part specifically lol
 		}
 	}
 	
-	// use CRUDBuddy to get a randomly chosen existing entry from sales table
-	private Object[] nextOldCustomer() throws SQLException {
-		return new Object[]{nextEmail(), "malcolm", "johnson", "80003"};
+
+	
+	private int nextQuantity() {
+		int gaussian = (int)nextGaussian() * 500;
+		int quantity = gaussian < 1 ? - 1 * gaussian : gaussian;
+		return quantity;
 	}
 	
-	private Object[] nextNewCustomer() {
-		return new Object[]{nextEmail(), "malcolm", "johnson", "80003"};
+	private Date nextDate() {
+		return new Date(dates.nextLine());
 	}
+	@org.jetbrains.annotations.NotNull
+	private Object[] nextCustomer()
+	throws SQLException {
+		int chance = (int)Math.round(nextGaussian()) * 10;
+		if(chance > 3 || chance < - 3) {
+			return nextNewCustomer();
+		} else {
+			return nextOldCustomer();
+		}
+	}	
+
+	private Object[] nextNewCustomer() {
+		String email = nextEmail();
+		String parts = (email.substring(0, email.indexOf("@")));
+		String firstName = parts.substring(0,parts.length() / 2); 
+		String lastName = parts.substring(parts.length()/2+1);
+		String location = ((nextInt() + 100000) % 10000) + "";
+		return new Object[] {email, firstName, lastName, location};
+	}
+	// use CRUDBuddy to get a randomly chosen existing entry from sales table
+	private Object[] nextOldCustomer() throws SQLException {
+		int index = nextInt(crud.query("select email from customers;").getMetaData()
+		 .getColumnCount() + 1);
+		ResultSet rs = crud.query("select * from customers where idx = " + index);
+		rs.next();
+		return new Object[] {rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4)};
+	}
+	
 	
 	public boolean shouldCreateNewCustomer(int i) {
 		int number = nextInt(1000000);
@@ -65,9 +89,13 @@ class RandomSim extends Random {
 	}
 	
 	public String nextProduct() {
-		return prodictIdList.get(nextInt(size));
+		return productIds[nextInt(size)];
 	}
+	
 	public String nextEmail() {
-		return "mjohn314@msudenver.edu";
+		if(! emailScanner.hasNextLine()) {
+			emailScanner = new Scanner("emails.csv");
+		}
+		return emailScanner.nextLine();
 	}
 }
