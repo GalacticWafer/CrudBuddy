cc1af693d1eb1557452c0990bf9f43e365d40ca5
Daniel Martinez <56371605+OutbreakSource@users.noreply.github.com>
2020-09-30 17:58:07 -0600

    Revert "Revert "Prieto export to csv""

--- src/main/java/Crud.java
+++ src/main/java/Crud.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.sql.*;
 import java.util.*;
+import java.util.Date;
 
 class Crud {
 	public static final int QUANTITY_SHORTAGE = - 2;
@@ -17,6 +18,14 @@ class Crud {
 	private static String DB_NAME;
 	private static Connection connection;
 	private String tableName = "inventory";
+	public static final int SALES = 0;
+	public static final int INVENTORY = 1;
+	public static final int CUSTOMERS = 2;
+	public static final Map<Integer, String[]> RECORD_STRINGS = Map.ofEntries(
+	 Map.entry(SALES, new String[] {"sales", "order_id"}),
+	 Map.entry(INVENTORY, new String[] {"inventory", "product_id"}),
+	 Map.entry(CUSTOMERS, new String[] {"customers", "email"})
+	);
 	
 	/**
 	 * Class that facilitates a connection to a database, and carries out CRUD
@@ -33,26 +42,26 @@ class Crud {
 		//JOptionPane.showMessageDialog(null, "Connection OK with " + getURL());
 	}
 	
-	/**Creates a .csv-compatible line from an ArrayList<String> */
+	/** Creates a .csv-compatible line from an String[] */
 	public String arrayToCSV
 	(String[] array) {
 		String a = Arrays.toString(array);
-		return a.substring(1, a.length() - 1);
+		return a.substring(1, a.length() - 1).replace(" ", "");
 	}
 	
-	/**Creates a gui to get user input on a new table to be uploaded to MySQL database. */
-	uploadCsvGui csvGuiLoad(String[] columns, String fileName) {
+	/** Creates a gui to get user input on a new table to be uploaded to MySQL database. */
+	private uploadCsvGui csvGuiLoad(String[] columns, String fileName) {
 		uploadCsvGui gui = new uploadCsvGui(columns, fileName, this);
 		gui.invoke();
 		return gui;
 	}
 	
-	/**Deletes all records from a table, but the table remains */
+	/** Deletes all records from a table, but the table remains */
 	public int deleteAllRecords(String table) throws SQLException {
 		return update("DELETE FROM " + table);
 	}
 	
-	/**Delete the record in the specified table */
+	/** Delete the record in the specified table */
 	public int deleteRecord
 	(String table, String idColumn, Object idValue)
 	throws SQLException {
@@ -60,12 +69,12 @@ class Crud {
 		 table, idColumn, idValue);
 	}
 	
-	/**Deletes an entire table */
+	/** Deletes an entire table */
 	public void deleteTable(String tableName) throws SQLException {
 		update("DROP TABLE IF EXISTS " + tableName);
 	}
 	
-	/**Find a specific record */
+	/** Find a specific record */
 	public Object[] find(String idValue, String idColumnName) throws SQLException {
 		String sql = format("select * from %s where %s = '%s';",
 		 tableName, idColumnName, idValue);
@@ -79,18 +88,18 @@ class Crud {
 		return record;
 	}
 	
-	/**Format a string for sql statement */
+	/** Format a string for sql statement */
 	public String format(String format, Object... args) {
 		return String.format(format, args);
 	}
 	
-	/**Get a ResultSet of an entire table */
+	/** Get a ResultSet of an entire table */
 	public ResultSet getAllRecords()
 	throws SQLException {
 		return query("SELECT * FROM " + tableName);
 	}
 	
-	/**Gets the number of columns in a table */
+	/** Gets the number of columns in a table */
 	public int getColumnCount(String tableName) throws SQLException {
 		setWorkingTable(tableName);
 		String sql =
@@ -103,27 +112,29 @@ class Crud {
 		return - 1;
 	}
 	
-	/**Gets an arraylist of the column names of a specific table */
+	/** Gets an arraylist of the column names of a specific table */
 	public String[] getColumnNames() throws SQLException {
-		// Gets an arraylist of the column names of a specific table
 		String sql = format(
 		 "SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS`" +
 		 " WHERE `TABLE_SCHEMA`='%s' AND `TABLE_NAME`='%s'", DB_NAME, tableName);
 		ResultSet rs = query(sql);
 		ArrayList<String> temp = new ArrayList<>();
-		int i = 0;
-		for(; rs.next(); i++) {
-			temp.add(rs.getString(1));
+		int size = 0;
+		while(rs.next()) {
+			if(! rs.getString(1).equals("idx")) {
+				temp.add(rs.getString(1));
+				size++;
+			}
 		}
-		String[] columnNames = new String[i];
+		String[] columnNames = new String[size];
 		Iterator<String> it = temp.iterator();
-		for(int i1 = 0; i1 < columnNames.length; i1++) {
+		for(int i1 = 0; i1 < size; i1++) {
 			columnNames[i1] = it.next();
 		}
 		return columnNames;
 	}
 	
-	/**Gets an arrayList of column types from a table */
+	/** Gets an arrayList of column types from a table */
 	public String[] getColumnTypes(String tableName)
 	throws SQLException {
 		ResultSet rs = query("select * from " + tableName + " where 1<0");
@@ -136,7 +147,7 @@ class Crud {
 		return list;
 	}
 	
-	/**Create the tuple of column names as a String to be sent as sql code. */
+	/** Create the tuple of column names as a String to be sent as sql code. */
 	public String getColumnsTuple(String[] columnNames) {
 		if(columnNames.length == 0) {return null;}
 		StringBuilder sb = new StringBuilder("(");
@@ -147,23 +158,23 @@ class Crud {
 		return sb.append(columnNames[i]).append(")").toString();
 	}
 	
-	/**Helper method to clean up code when concatenating commas for sql code. */
+	/** Helper method to clean up code when concatenating commas for sql code. */
 	private static String getComma(int length, int i, String lastChar) {
 		if(i < length - 1) {return ", ";}
 		return lastChar + "";
 	}
 	
-	/**Return the database name */
+	/** Return the database name */
 	public Object getDatabaseName() {
 		return DB_NAME;
 	}
 	
-	/**Get an array with all the table names */
+	/** Get an array with all the table names */
 	public String[] getTableNames() throws SQLException {
 		ResultSet rs = query("SHOW tables");
 		int count = 0;
 		ArrayList<String> temp = new ArrayList<>();
-		while(rs.next()){
+		while(rs.next()) {
 			count++;
 			temp.add(rs.getString(1));
 		}
@@ -175,28 +186,49 @@ class Crud {
 		return tableNames;
 	}
 	
-	/**Get the sql string representing the name of a java data type */
+	/** Get the sql string representing the name of a java data type */
 	public String getType(String name) { return uploadCsvGui.J_TO_SQL2.get(name); }
 	
-	/**Get the URL by joining static variable together. */
+	/** Get the URL by joining static variable together. */
 	private static String getURL() {
 		return "jdbc:mysql://" + HOST_IP + ":" + PORT + "/" + DB_NAME;
 	}
 	
-	/**Insert one or more new records into a table */
-	public int insertRecords(String[] columnNames, String[][] tableValues)
-	throws SQLException {
+	/** Create a new record in the specified table from order information. */
+	public void insertFromOrder(Order od, int table) throws SQLException {
+		String[] recordStrings = RECORD_STRINGS.get(table);
+		setWorkingTable(recordStrings[0]);
+		if(!exists(recordStrings[1], od.getMatchValue(table))) {
+			insertRecords(getColumnNames(), new Object[][] {od.toArray(table)});
+		}
+	}
+	
+	/** Insert one or more new records into a table from a 2d array. */
+	public int insertRecords(String[] columnNames, Object[][] tableValues)
+	throws InputMismatchException {
+		if(tableValues.length == 0 || columnNames.length == 0) {
+			throw new InputMismatchException();
+		}
 		StringBuilder sb = new StringBuilder(format(
 		 "INSERT INTO %s %s VALUES",
 		 tableName, getColumnsTuple(columnNames)));
 		for(int i = 0; i < tableValues.length; i++) {
-			sb.append(toValueTuple(tableValues[i], null));
+			if(tableValues[i].length != columnNames.length) {
+				throw new InputMismatchException();
+			}
+			sb.append(toValueTuple(tableValues[i]));
 			sb.append(i == tableValues.length - 1 ? ";" : ",");
 		}
-		return update(sb + "");
+		try {
+			return update(sb + "");
+		}
+		catch(Exception e) {
+			System.out.println(sb + " must have a huge email address...");
+			return - 1;
+		}
 	}
 	
-	/**Creates a blank Table */
+	/** Creates a blank Table */
 	public void insertTable
 	(String tableName, String[] columnNames, HashMap<Integer, String> typeMap)
 	throws SQLException {
@@ -232,13 +264,15 @@ class Crud {
 		csvGuiLoad(columns, filePath);
 	}
 	
-	/**Return an integer indicating if the order can be processed, and if not, why */
+	/** Return an integer indicating if the order can be processed, and if not, why */
 	public int isProcessableOrder(Order order) throws SQLException, MessagingException {
+		setWorkingTable("inventory");
 		String productId = order.getProductId();
 		ResultSet rs = queryF("select quantity from %s where product_id = '%s';",
 		 tableName, productId);
 		if(! rs.next()) {
 			order.setResultString(order.getProductId() + " : unknown product id");
+			System.out.println(order.getProductId() + " : unknown product id");
 			return UNKNOWN_PRODUCT;
 		}
 		int currentQuantity = rs.getInt(1);
@@ -252,23 +286,39 @@ class Crud {
 		return currentQuantity;
 	}
 	
-	/**Sends a sql query string */
+	/** Sends a sql query string */
 	public ResultSet query(String query) throws SQLException {
 		return connection.createStatement().executeQuery(query);
 	}
 	
-	/**Formats, then sends a sql query string */
+	/** Formats, then sends a sql query string */
 	public ResultSet queryF(String format, Object... args) throws SQLException {
 		return query(format(format, args));
 	}
 	
-	/**Wraps the given object in quotes if it is a string */
+	/** Wraps the given object in quotes if it is a string */
 	private static String quoteWrap(Object columnValue) {
-		if(columnValue instanceof String) {return "'" + columnValue + "'";}
+		if(columnValue instanceof String 
+		   || columnValue instanceof Date) {
+			return "'" + columnValue + "'";
+		}
 		return columnValue.toString();
 	}
 	
-	/**Removes the (BOM byte-order mark) from the beginning of the string. */
+	private boolean exists(String columnName, Object columnValue) throws SQLException {
+		ResultSet rs = queryF(
+		 "SELECT EXISTS(SELECT * FROM %s WHERE %s = %s);", tableName,
+		 columnName, quoteWrap(columnValue));
+		return rs.getFetchSize() > 0;
+	}
+	
+	/** Restock a given product. */
+	public void restock(String productId) throws SQLException {
+		updateRow(new String[] {"quantity"}, new Object[] {500}, "product_id", productId,
+		 "inventory");
+	}
+	
+	/** Removes the (BOM byte-order mark) from the beginning of the string. */
 	private static String removeUTF8BOM(String s) {
 		if(s.startsWith("\uFEFF")) {
 			s = s.substring(1);
@@ -276,44 +326,71 @@ class Crud {
 		return s;
 	}
 	
-	/**Updates a product's quantity from an order */
+	/** retrieve the number of rows of a column. */
+	int size() throws SQLException {
+		ResultSet rs = query("SELECT COUNT(*) FROM " + tableName);
+		rs.next();
+		return rs.getInt(1);
+	}
+	
+	/** Updates a product's quantity from an order */
 	public void setQuantityFromOrder(Order order) throws SQLException {
 		int newQuantity = order.getCurrentQuantity() - order.getQuantity();
 		updateF("UPDATE %s SET quantity = %s WHERE product_id = '%s';",
 		 tableName, newQuantity, order.getProductId());
 	}
 	
-	/**Sets the static variable <code>tableName</code> as the table to make statements against. */
+	/** Sets the static variable <code>tableName</code> as the table to make statements against. */
 	public void setWorkingTable(String tableName) { this.tableName = tableName; }
 	
-	/**Makes a tuple of values from an array for sql statements */
-	public String toValueTuple(String[] array, HashMap<Integer, String> typeMap) {
+	/** Makes a tuple of values from an array for sql statements */
+	public String toValueTuple(Object[] array, HashMap<Integer, String> typeMap) {
+		if(typeMap == null) {
+			throw new NullPointerException();
+		}
 		Object[] values = new Object[array.length];
 		StringBuilder sb = new StringBuilder("(");
 		for(int i = 0; i < values.length; i++) {
-			if(typeMap != null && typeMap.get(i).contains("CHAR")) {
+			if(shouldQuote(typeMap.get(i))){
 				array[i] = "'" + array[i] + "'";
 			}
 			sb.append(array[i]);
 			if(i == values.length - 1) {
 				sb.append(")");
-			}
-			else {sb.append(",");}
+			} else {sb.append(",");}
 		}
 		return sb.toString();
 	}
 	
-	/**Send an sql executeUpdate() statement*/
+	private boolean shouldQuote(String s) {
+		return s.contains("VARCHAR")
+		|| s.contains("DATE");
+	}
+	
+	/** Makes a tuple of values from an array for sql statements */
+	public String toValueTuple(Object[] array) {
+		Object[] values = new Object[array.length];
+		StringBuilder sb = new StringBuilder("(");
+		for(int i = 0; i < values.length; i++) {
+			sb.append(quoteWrap(array[i]));
+			if(i == values.length - 1) {
+				sb.append(")");
+			} else {sb.append(",");}
+		}
+		return sb.toString();
+	}
+	
+	/** Send an sql executeUpdate() statement */
 	public int update(String sql) throws SQLException {
 		return connection.createStatement().executeUpdate(sql);
 	}
 	
-	/**Format an sql executeUpdate() statement*/
+	/** Format an sql executeUpdate() statement */
 	public int updateF(String format, Object... args) throws SQLException {
 		return update(format(format, args));
 	}
 	
-	/**Update a row where some column name = some value*/
+	/** Update a row where some column name = some value */
 	public void updateRow
 	(String[] columns, Object[] values, Object columnName, String columnValue, String tableName)
 	throws SQLException {
@@ -329,7 +406,7 @@ class Crud {
 		update(sf + "");
 	}
 	
-	/**Write a table from the database to a file */
+	/** Write a table from the database to a file */
 	public File writeToFile
 	(String path, String[] columns, ResultSet results)
 	throws FileNotFoundException, SQLException {
@@ -347,4 +424,24 @@ class Crud {
 		pw.close();
 		return file;
 	}
+
+	/**Get a ResultSet of an entire table */
+	public ResultSet getRecord(String columnVal, String columnName)
+			throws SQLException
+	{
+		return query("SELECT * FROM " + tableName + " where " + columnName +
+				" equals " + columnVal);
+	} // End getRecord
+
+	protected Boolean isClosed() throws SQLException {
+		return Crud.connection.isClosed();
+	}
+
+	protected String getWorkingTable() {
+		return this.tableName;
+	}
+	
+	public static final String[] INVENTORY_COLUMNS = new String[] {"product_id", "quantity", "wholesale_cost", "sale_price", "supplier_id"};;
+	//public static final String[] SALES_COLUMNS = new String[] {"product_id", "quantity", "wholesale_cost", "sale_price", "supplier_id"};;
+	//public static final String[] CUSTOMERS_COLUMNS = new String[] {"product_id", "quantity", "wholesale_cost", "sale_price", "supplier_id"};;
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,38 +5,45 @@ import java.sql.SQLException;
 import java.util.*;
 
 public class Main {
-		public static final String[] COLUMN_NAMES =
-	 new String[] {"product_id", "quantity", "wholesale_cost", "sale_price", "supplier_id"};
-
-	
 	public static void main(String[] args)
-	throws SQLException, ClassNotFoundException, FileNotFoundException {
+	 throws SQLException, ClassNotFoundException, FileNotFoundException
+	{
+		//Crud crud = Credentials.databaseLogin();
+		//GUI GUI = new GUI(crud);
+		//new RandomSimTest();
+		
 		Crud crud = Credentials.databaseLogin();
-		crud.setWorkingTable("inventory");
-		crud.writeToFile("new_test.csv", crud.getColumnNames(), crud.getAllRecords());
+		String FileName = "gui.csv";
+		GUI GUI = new GUI(crud);
 	}
 	
-	private static void insertNewRecord(Crud crud, String tableName) throws SQLException {
+	private static void insertNewRecord(Crud crud, String tableName) throws SQLException
+	{
 		crud.setWorkingTable(tableName);
-		Object[] record = crud.find("VBEUS2ETCKA4","product_id");
-		System.out.println(Arrays.toString(record));
+		Object[] insertFromOrder = crud.find("VBEUS2ETCKA4", "product_id");
+		System.out.println(Arrays.toString(insertFromOrder));
 		String someTextField = "sales";
 		int columnCount = crud.getColumnCount(someTextField);
 		System.out.println(columnCount);
 		Object[] newRecord = new Object[columnCount];
-		for(int i = 0; i < newRecord.length; i++) {
-			newRecord[i] = null/*fixme, insertRecord all the names of your JtextFields and such variables*/;
+		for(int i = 0; i < newRecord.length; i++)
+		{
+			newRecord[i] = null
+			//fixme, insertRecord all the names of your JtextFields and such variables
+			;
 		}
 	}
 	
 	private static void dailyOrderCheck(Crud crud)
-	throws MessagingException, IOException, SQLException {
+	 throws MessagingException, IOException, SQLException
+	{
 		
 		LinkedList<int[]> list = Emailer.processEmailOrders(crud);
 		System.out.println(list);
 	}
 	
-	private static void orderDemoSetup(Crud crud) throws SQLException {
+	private static void orderDemoSetup(Crud crud) throws SQLException
+	{
 		
 		crud.setWorkingTable("inventory");
 		Random rand = new Random();
@@ -58,13 +65,15 @@ public class Main {
 		 "J575EG1KGXDI", "NBGSOV8WIZ54",
 		 "VJTR3EQFQZ5E", "6TWB0AUWETK7"
 		};
-		for(String testId: testIds) {
+		for(String testId: testIds)
+		{
 			crud
 			 .updateRow(new String[] {"quantity"}, new Object[] {rand.nextInt(2000)},
 			  "product_id", testId, "inventory");
 		}
 		
-		for(String goodId: goodIds) {
+		for(String goodId: goodIds)
+		{
 			crud
 			 .updateRow(new String[] {"quantity"}, new Object[] {999999999},
 			  "product_id", goodId, "inventory");
