a28750279ade5b23ae30f581c0284f1081cf2ce1
OutbreakSource <daniel_ends@yahoo.com>
2020-10-19 20:18:42 -0600

    -fixed date for emailer TransactionItem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
 	public static void main(String[] args)
 	throws Exception {
 		Crud crud = Credentials.databaseLogin();
+		Emailer.processEmailOrders(crud);
 		//new GUI(crud);
 		// topNByCustomer()
 		/*
