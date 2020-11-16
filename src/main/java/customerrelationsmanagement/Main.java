4b586e3e2277ef59b5f5885be943c61084df6580
GalacticWafer <mxhjohnson4dba@gmail.com>
2020-11-16 01:37:40 -0700

    - Added Tables.java enum to remove constants floating around in different classes - Added Todo's for implementing the new supplier table functionality - removed depricated Crud.exists and Crud.updateRow methods, and associated tests

--- src/main/java/customerrelationsmanagement/Main.java
+++ src/main/java/customerrelationsmanagement/Main.java
@@ -2,27 +2,27 @@ package customerrelationsmanagement;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Main {
-	//public static final String INVENTORY_PATH = "inventory_team4.csv";
-	public static final String INVENTORY_PATH = null;
-	
-	//public static final String analyticsDir = "analytics";
-	public static final String analyticsDir = null;
-	
-	//public static final String ORDERS_PATH = "customer_orders_A_team4.csv";
-	public static final String ORDERS_PATH = "";
-	
-	public static final boolean START_GUI = true;
-	//public static final boolean START_GUI = false;
-	
-	public static final boolean START_MAIL = true;
-	//public static final boolean START_MAIL = true;
-	
+	public static final String INVENTORY_PATH = "inventory_team4.csv";
+	public static final String ORDERS_PATH =
+	 "customer_orders_A_team4.csv";
+	//"customer_orders_A_team4_1000x.csv";
+	//null;
+	public static final boolean START_GUI =
+	 true;
+	//false;
+	public static final boolean START_MAIL =
+	 // true;
+	 false;
+	public static final String analyticsDir =
+	 //"analytics";
+	 null;
 	private static Credentials credentials;
 	private static Crud crud;
 	private static Emailer mailer;
@@ -30,21 +30,19 @@ public class Main {
 	private static Restoration rest;
 	
 	public Main(Credentials credentials, Crud queryMaker)
-			throws IOException, SQLException, ParseException {
+	throws IOException, SQLException, ParseException {
 		startServices(credentials);
 	}
 	
 	public static void main(String[] args) {
 		
-		System.out.println("This is the first line of main.");
 		credentials = new Credentials();
 	}
 	
-	
 	public void startServices(Credentials credentials)
-			throws IOException, SQLException, ParseException {
+	throws IOException, SQLException, ParseException {
 		
-		this.crud = credentials.getCrud();
+		crud = credentials.getCrud();
 		if(START_MAIL) {
 			mailer = new Emailer(credentials);
 			Timer timer = new Timer();
@@ -59,13 +57,35 @@ public class Main {
 			}, 5000);
 		}
 		if(INVENTORY_PATH != null && !INVENTORY_PATH.equals("")) {
-			rest = new Restoration(crud, INVENTORY_PATH, "customer_orders_A_team4.csv",true, analyticsDir);
+			long start = System.nanoTime();
+			rest =
+			 new Restoration(crud, INVENTORY_PATH, "customer_orders_A_team4" +
+												   ".csv", true, analyticsDir);
+			long l = System.nanoTime() - start;
+			System.out.printf(
+			 "%.2f seconds to delete the database and start over the gui\n",
+			 (l / 1000000000.0));
 		}
 		if(START_GUI) {
+			long start = System.nanoTime();
 			new GUI(crud);
+			long l = System.nanoTime() - start;
+			System.out.printf(
+			 "%.2f seconds to build the gui\n", (l / 1000000000.0));
 		}
 		if(ORDERS_PATH != null && !ORDERS_PATH.equals("")) {
+			long start = System.nanoTime();
 			OrderProcessor.runFileOrders(crud, ORDERS_PATH);
+			long l = System.nanoTime() - start;
+			System.out
+			 .printf("%.2f seconds to process and insert %d new records", (l /
+																		   1000000000.0),
+			  (Integer.parseInt(crud
+			   .getRecords("select count(*) from " + Tables.STATUSED)[0][0]
+			   .toString())) +
+			  (Integer.parseInt(crud
+			   .getRecords("select count(*) from " + Tables.ANALYTICS)[0][0]
+			   .toString())));
 		}
 	}
 }
