447e51b4b7f5bb8d6fe85dae9a1b3b9489a34cb0
Uriel B <59666398+ubello64@users.noreply.github.com>
2020-11-17 20:36:30 -0700

    created a new Enum for system performance ChartMaker added new test files to test on a scale new getChart method for time series chart temporary changes to main to test new plot

--- src/main/java/customerrelationsmanagement/Main.java
+++ src/main/java/customerrelationsmanagement/Main.java
@@ -1,18 +1,25 @@
 package customerrelationsmanagement;
 
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.util.Timer;
 import java.util.TimerTask;
+import javax.swing.*;
 
 public class Main {
 	public static final String INVENTORY_PATH = "inventory_team4.csv";
 	public static final String ORDERS_PATH =
-	 "customer_orders_A_team4.csv";
-	//"customer_orders_A_team4_1000x.csv";
+	 "customer_orders_A_team4.csv,"+
+	// "customer_orders_A_team4_x3.csv,"+
+	"customer_orders_A_team4_x5.csv," +
+	  "customer_orders_A_team4_x7.csv";
 	//null;
 	public static final boolean START_GUI =
 	 true;
@@ -74,18 +81,36 @@ public class Main {
 			 "%.2f seconds to build the gui\n", (l / 1000000000.0));
 		}
 		if(ORDERS_PATH != null && !ORDERS_PATH.equals("")) {
-			long start = System.nanoTime();
-			OrderProcessor.runFileOrders(crud, ORDERS_PATH);
-			long l = System.nanoTime() - start;
-			System.out
-			 .printf("%.2f seconds to process and insert %d new records", (l /
-																		   1000000000.0),
-			  (Integer.parseInt(crud
-			   .getRecords("select count(*) from " + Tables.STATUSED)[0][0]
-			   .toString())) +
-			  (Integer.parseInt(crud
-			   .getRecords("select count(*) from " + Tables.ANALYTICS)[0][0]
-			   .toString())));
+			String[] fileNames = ORDERS_PATH.split(",");
+			long[] timeValues = new long[fileNames.length];
+			for (int i = 0; i < fileNames.length; i++) {
+				long start = System.nanoTime();
+				OrderProcessor.runFileOrders(crud, fileNames[i]);
+				long end = System.nanoTime();
+				long timeInterval = end - start;
+				System.out
+				 
+				 .printf("%.2f seconds to process and insert %d new records", (
+				   timeInterval /
+				   1000000000.0),
+				  (Integer.parseInt(crud
+				   .getRecords("select count(*) from " + Tables.STATUSED)[0][0]
+				   .toString())) +
+				  (Integer.parseInt(crud
+				   .getRecords("select count(*) from " + Tables.ANALYTICS)[0][0]
+				   .toString())));
+				timeValues[i] = timeInterval;
+			}
+			JFrame frame = new JFrame("Speed Test");
+			ChartMaker maker = new ChartMaker(crud);
+			JFreeChart chart = maker.getChart("string", ChartType.SPEED_TEST, timeValues,
+			 new int[]{1455,7275,10185});
+			ChartPanel cp = new ChartPanel(chart);
+			frame.getContentPane().add(cp);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setLocationRelativeTo(null);
+			frame.pack();
+			frame.setVisible(true);
 		}
 	}
 }
