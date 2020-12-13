2dcb87a829e97dd88446ad2d5887a24d009b1be1
GalacticWafer <48652354+GalacticWafer@users.noreply.github.com>
2020-12-12 23:50:56 -0700

    Update App.js

--- client_/src/App.js
+++ client_/src/App.js
@@ -1,28 +1,22 @@
-import React, { Component,useEffect,useState } from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import './App.css';
 import Axios from 'axios';
 import Main from './components/main';
-
-
+import {Link} from 'react-router-dom';
 
 import
 {
-Collapse,
-Navbar,
-NavbarToggler,
-NavbarBrand,
-Nav,
-NavItem,
-NavLink,
-Container
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    Nav,
+    NavItem,
+    Container
 } from 'reactstrap';
-import { userInfo } from 'os';
-
-
+import {userInfo} from 'os';
 
 
 class App extends Component {
-
 
 
 // state = {
@@ -38,7 +32,7 @@ class App extends Component {
 
 //   getInverntorydata = ()=> {
 //     Axios.get("http://localhost:3306/").
-//     then((response) =>{
+//     then((response) =>{m
 //       const data = response.data;
 //       this.setState({ posts: data});
 //       console.log(data[0]);
@@ -48,61 +42,52 @@ class App extends Component {
 //   }
 
 
-  state = {
-    isOpen: false 
-}
+    state = {
+        isOpen: false
+    }
 
-toggle = () =>
-{
-    this.setState(
-        {
-            isOpen: !this.state.isOpen
-        });
-}
-
+    toggle = () => {
+        this.setState(
+            {
+                isOpen: !this.state.isOpen
+            });
+    }
 
 
-  render() {
-    return (
-     
-      <div className="App">
- <Navbar id="navbar" font="" height="px;"  expand="sm" className="mb-0">
-                <Container>
-                    <NavbarBrand href="/">
-                    
-       <span id="logoimage"></span>
-
-                    </NavbarBrand>
-                    <NavbarToggler onClick = {this.toggle} ></NavbarToggler>
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav className="ml-auto" height="0px">
-                    <NavItem>
-                        <NavLink href="home">
-                           Home
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink height="50px;" href="shop">
-                        Shop
-                        </NavLink>
-                        
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="about">
-                       About 
-                        </NavLink>
-                        
-                    </NavItem>
-                    </Nav>
-                    </Collapse>
-                </Container>
-            </Navbar>
-      
-
-<Main/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Navbar id="navbar" font="" height="px;" expand="sm" className="mb-0">
+                    <Container>
+                        <Link to="/">
+                            <span id="logoimage"></span>
+                        </Link>
+                        <NavbarToggler onClick={this.toggle}></NavbarToggler>
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" height="0px">
+                                <NavItem>
+                                    <Link to="home">
+                                        Home
+                                    </Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link height="50px;" to="shop">
+                                        Shop
+                                    </Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to="about">
+                                        About
+                                    </Link>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+                <Main/>
+            </div>
+        );
+    }
 }
 
 export default App;
